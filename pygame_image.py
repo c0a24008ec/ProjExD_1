65ba8ee2e08688a66549cc841f86e9c166f83b56
HBK.A <c0a24008ec@edu.teu.ac.jp>
2025-06-10 15:53:06 +0900

    ex.10

--- pygame_image.py
+++ pygame_image.py
@@ -12,9 +12,11 @@ def main():
     bg_img1 = pg.image.load("fig/pg_bg.jpg")
     bg_img_flip = pg.transform.flip(bg_img1, True, False)
     bg_img2 = pg.image.load("fig/pg_bg.jpg")
-    bird_img = pg.image.load("fig/3.png")
-    bird_img = pg.transform.flip(bird_img, True , False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True , False)
     tmr = 0
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -22,7 +24,16 @@ def main():
         screen.blit(bg_img1, [-tmr, 0])
         screen.blit(bg_img_flip, [-tmr+1600, 0])
         screen.blit(bg_img2, [-tmr+3200, 0])
-        screen.blit(bird_img,[300,200])
+        screen.blit(kk_img,kk_rct)
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
         if tmr > 3199:
             tmr = 0
         pg.display.update()
